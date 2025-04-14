29df8fe08327d8270bb0ac3ab7e8f6bcd5b018bb
Lim Vitaly <yifanset2602@gmail.com>
2025-04-14 20:29:20 +0700

    media for main abd qblock1

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import './App.css';
 import PageHeader from './components/page/PageHeader';
 import MainBlock from './components/block/main-block/MainBlock';
 import Quiz1Block from './components/block/quiz1-block/Quiz1Block';
-import Quiz2Block from './components/block/quiz2-block/Quiz2Block';
-import ResultsBlock from './components/block/results-block/ResultsBlock';
 
 function App() {
   return (
@@ -11,8 +9,6 @@ function App() {
       <PageHeader/>
       <MainBlock/>
       <Quiz1Block/>
-      <Quiz2Block/>
-      <ResultsBlock/>
     </>
   )
 }
