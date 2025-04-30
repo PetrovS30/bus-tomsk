653ceef1e91daaa1f6461428d197597e4c9e0f91
Сергей <Resler18@yandex.com>
2025-05-01 01:17:37 +0300

    make layout

--- src/components/block/results-block/ResultsBlock.jsx
+++ src/components/block/results-block/ResultsBlock.jsx
@@ -1,82 +1,56 @@
 import { useState } from 'react';
 import './results-block.css';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Label} from 'recharts';
-
-const data = [
-    { name: 1, "всего оценок": 20 },
-    { name: 2, "всего оценок": 22 },
-    { name: 3, "всего оценок": 90 },
-    { name: 4, "всего оценок": 50 },
-    { name: 5, "всего оценок": 144 }
-  ];
-
-  const CustomBar = ({ x, y, width, height, fill }) => {
-    const strokeColor = '#3f5f95'; // Цвет обводки
-    const strokeWidth = 2; // Ширина обводки
-    return (
-      <rect
-      stroke={strokeColor} // Цвет обводки
-      strokeWidth={strokeWidth} // Ширина
-          x={x + 5} // Устанавливаем отступ слева
-          y={y}
-          width={width - 10} // Уменьшаем ширину для создания отступа справа
-          height={height}
-          fill={fill}
-      />
-  );
-  };
-
-  const MyBarChart = () => {
+import leftIcon from './free-icon-left.svg';
+import rightIcon from './free-icon-right.svg';
+import MyBarChart1 from './MyBarChart1';
+import MyBarChart2 from './MyBarChart2';
+import MyBarChart3 from './MyBarChart3';
 
 
-
-    const customTicks = [0, 50, 100, 150,  200];
-    return (
-    <div style={{ backgroundColor: '#fff', margin:" 0 auto",marginTop: "74px", width: "500px", height: "300px", display: 'flex'}}>
-
-        <BarChart barCategoryGap={0} margin={{ top: 20, right: 30, left: 20, bottom: 5 }} width={500} height={300} data={data}  >
-        <CartesianGrid strokeDasharray="3 3" fill="" stroke="none" />{/*фон */}
-        <XAxis dataKey="name" stroke="#000" strokeWidth={1}>
-            <Label offset={0} position="insideBottom" />
-        </XAxis>
-        <XAxis stroke="#FFFF" strokeWidth={15} dataKey="name" />
-        <YAxis ticks={customTicks}  />
-        <Tooltip />
-        <Legend />
-        <Bar  minPointSize={0} shape={<CustomBar />} barSize={100} stroke="#000" dataKey="всего оценок" fill="#ffc658" />
-      </BarChart>
-
-        <BarChart barCategoryGap={0} margin={{ top: 20, right: 30, left: 20, bottom: 5 }} width={500} height={300} data={data}  >
-        <CartesianGrid strokeDasharray="3 3" fill="" stroke="none" />{/*фон */}
-        <XAxis dataKey="name" stroke="#000" strokeWidth={1}>
-            <Label offset={0} position="insideBottom" />
-        </XAxis>
-        <XAxis stroke="#FFFF" strokeWidth={15} dataKey="name" />
-        <YAxis ticks={customTicks}  />
-        <Tooltip />
-        <Legend />
-        <Bar  minPointSize={0} shape={<CustomBar />} barSize={100} stroke="#000" dataKey="всего оценок" fill="#ffc658" />
-      </BarChart>
-
-    </div>
-    )
-  };
-  
 
 const ResultsBlock = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + 2) % 2);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const charts = [
+    <MyBarChart1 key={0}/>,
+    <MyBarChart2 key={1}/>,
+    <MyBarChart3 key={2}/>,
+  ];
+
+  const questions = [
+    'Качество автобусов',
+    'Движение транспорта',
+    'Чисто в транспорте?'
+  ];
+
+  const handlePrevClick = () => {
+    setActiveIndex((prevIndex) => (prevIndex - 1 + charts.length) % charts.length);
   };
 
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % 2);
+  const handleNextClick = () => {
+    setActiveIndex((prevIndex) => (prevIndex + 1) % charts.length);
   };
+
     return (
         <div className='result-block__block'>
-                  <button onClick={handlePrev}>prev</button><button onClick={handleNext}>next</button>
-            <MyBarChart/>
+          <div className='result-block__block-left'>
+            <div className=' result-block__block-left-results'>Результаты</div>
             <h2>Спасибо за ответы!</h2>
+            <button>Обновить данные</button>
+          </div>
+          <div className='result-block__block-right'>
+          {charts[activeIndex]}
+            <h2 style={{margin: '0 auto', width: "418px", textAlign: "center", color: 'black', fontSize: 40}}>{questions[activeIndex]}</h2>
+            <div className='result-block__block-right-paginations'>
+              <ul style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                <li><img src={rightIcon} alt="rightIcon" onClick={handlePrevClick} /></li>
+                <li><button className='result-block__block-right-paginations-btn'></button></li>
+                <li><button className='result-block__block-right-paginations-btn'></button></li>
+                <li><button className='result-block__block-right-paginations-btn'></button></li>
+                <li><img src={leftIcon} alt="leftIcon" onClick={handleNextClick}/></li>
+              </ul>
+            </div>
+          </div>
         </div>
     )
 } 
